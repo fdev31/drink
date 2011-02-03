15844dcd8e2204a93a420c1fe61a2a1dbe9dcd02
fab <fdev31@gmail.com>
2011-02-03 14:49:20 +0100

    fix db open/closing

--- drink/zdb.py
+++ drink/zdb.py
@@ -10,16 +10,16 @@ class Database(IterableUserDict):
         self._storage = storage
         self.app.add_hook('before_request', self.open_db)
         self.app.add_hook('after_request', self.close_db)
+        self.connection = None
 
     def open_db(self):
-        self.connection = self.db.open()
-        self.data = self.connection.root()
+        if not self.connection:
+            self.connection = self.db.open()
+            self.data = self.connection.root()
         transaction.begin()
 
     def close_db(self):
         transaction.commit()
-        del self.data
-        self.connection.close()
 
     @property
     def db(self):
