50fadddb8bc349d0e60825fbb6cdf9c9cc69b30d
fab <fdev31@gmail.com>
2011-02-08 21:34:26 +0100

    chec db size on startup to auto-init
    --HG--
    branch : xp-ajax

--- runserver.py
+++ runserver.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
-    from drink import startup
+    from drink import init, startup, db
+    if len(db.db.open().root()) == 0:
+        init()
     startup()
