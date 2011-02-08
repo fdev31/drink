844ee068e77d9edb177f2f018e10020c614ef218
fab <fdev31@gmail.com>
2011-02-08 23:21:23 +0100

    protect db from reload
    --HG--
    branch : xp-ajax

--- runserver.py
+++ runserver.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import os
+from drink import init, startup, db
 
 if __name__ == "__main__":
-    from drink import init, startup, db
-    if len(db.db.open().root()) < 3: # users, groups + pages at least
-        init()
+    if not 'BOTTLE_CHILD' in os.environ:
+        if len(db.db.open().root()) < 3: # users, groups + pages at least
+            init()
+
     startup()
