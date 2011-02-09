ca7c27e18558c537b8cab758943c9adde903aaaf
fab <fdev31@gmail.com>
2011-02-09 12:58:24 +0100

    remove old task* stuff
    --HG--
    branch : xp-ajax

--- drink/objects/tasks.py
+++ drink/objects/tasks.py
@@ -15,7 +15,6 @@ class TODO(drink.Page):
     }
 
     content = ''
-    title = ''
 
     def view(self):
         drink.rdr(self.path+'edit')
@@ -43,107 +42,4 @@ class TODOList(drink.ListPage):
     </div>
     """
 
-class TasksPage(drink.Page):
-
-    mime = "tasks"
-
-    doc = "A Task list"
-
-    css = [
-        "/static/style.css",
-    ]
-
-    js = [
-        "/static/jquery.shoppingList.js",
-        "/static/script.js",
-    ]
-
-    title = "A task lsit"
-
-    html = """
-    <div><a href="../"><img class="icon" src="/static/back.png" />Exit!</a></div>
-
-    <h1>Daily tasks</h1>
-    <div class="shoppingList">
-        <ul>
-        </ul>
-    </div>
-    """
-
-    def __init__(self, name, rootpath):
-        drink.Page.__init__(self, name, rootpath)
-        self.name = name
-
-    def struct(self):
-        return [t.struct() for t in self.itervalues()]
-
-    def view(self):
-        fmt  = drink.request.GET.get('format', 'html')
-        if fmt == 'json':
-            return dict(tasks=self.struct())
-        return drink.template('main.html', obj=self, no_admin=True,
-             js=self.js, css=self.css, html=self.html, authenticated=drink.request.identity)
-
-    def add(self):
-        if 'w' not in drink.request.identity.access(self):
-            return drink.abort(401, "Additions Not authorized")
-
-        content = drink.request.GET.get('text')
-        t = Task(sha1(content or str(time.time())).hexdigest(), self.path, content)
-        self[t.id] = t
-        transaction.commit()
-        return t.id
-
-    def rm(self):
-        if 'w' not in drink.request.identity.access(self):
-            return drink.abort(401, "Deletion Not authorized")
-
-        tid = drink.request.GET.get('id')
-        del self[tid]
-        transaction.commit()
-        return 'ok'
-
-    # FIXME: rewrite, this should be default "add" method
-    def edit(self):
-        if 'w' not in drink.request.identity.access(self):
-            return drink.abort(401, "Edition Not authorized")
-
-        tid = drink.request.GET.get('id')
-        content = drink.request.GET.get('text')
-        self[tid].content = content
-        transaction.commit()
-        return 'ok'
-
-
-class Task(drink.Model):
-
-    editable_fields = {
-        'content': drink.types.Text(),
-    }
-
-    @property
-    def title(self):
-        if len(self.content) > 20:
-            return self.content[:20]+"..."
-        else:
-            return self.content
-
-    @property
-    def path(self):
-        return self.rootpath + self.id
-
-    def __init__(self, name, rootpath, content=''):
-        drink.Model.__init__(self, name, rootpath)
-        self.content = content
-        self.created_on = time.time()
-
-    def view(self):
-        return '<div id="%(id)s">%(text)s</div>'%self.get()
-
-    def struct(self):
-        return {
-            'id': self.id,
-            'text': self.content,
-        }
-
-exported = {'Task list': TasksPage, 'TODO list': TODOList}
+exported = {'TODO list': TODOList}
