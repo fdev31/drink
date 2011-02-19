90bf13127a8dcb8540c3d9648e24277e98abd3b8
fab <fdev31@gmail.com>
2011-02-19 23:43:53 +0100

    do not allow traversal of elements starting with an underscore

--- drink/objects/__init__.py
+++ drink/objects/__init__.py
@@ -30,6 +30,8 @@ def get_object(current, objpath):
         else:
             # traversal
             try:
+                if elt.startswith('_'):
+                    abort(401, 'Not authorized')
                 current = current[elt]
                 if 't' not in request.identity.access(current):
                     abort(401, 'Not authorized')
