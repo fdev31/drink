d3f68bd72495326de8e868deb5a899f825f50177
fab <fdev31@gmail.com>
2011-07-30 18:45:26 +0200

    Cosmetic change

--- drink/config.py
+++ drink/config.py
@@ -7,6 +7,8 @@ import ConfigParser
 BASE_DIR = os.path.abspath(os.path.split(__file__)[0])
 
 config = ConfigParser.ConfigParser()
+paths = [os.path.join(BASE_DIR, 'settings.ini'),
+     os.path.expanduser('~/.drink.ini')]
 
-config.read([os.path.join(BASE_DIR, 'settings.ini'),
-     os.path.expanduser('~/.drink.ini')])
+print "Reading config from %s"%(' and '.join(paths))
+config.read(paths)
