5bc062fe9e5eee5d2f8b2282d88460c13c78f28e
fab <fdev31@gmail.com>
2011-12-04 22:22:24 +0100

    Silent fix

--- drink/objects/markdown.py
+++ drink/objects/markdown.py
@@ -85,7 +85,7 @@ class MarkdownPage(drink.ListPage):
     $(document).ready(function(){
         m = new MarkDown();
         m.load_page();
-        add_hook_add_item(m.load_page);
+        add_hook_add_item(function() {m.load_page});
     });
     ''']
 
