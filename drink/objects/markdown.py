605d948b6652c45f916607fb1f368abc0755c092
fab <fdev31@gmail.com>
2011-12-04 22:22:40 +0100

    Blog entries are row style now

--- drink/objects/markdown.py
+++ drink/objects/markdown.py
@@ -179,7 +179,7 @@ class MarkdownPage(drink.ListPage):
             items.sort(key=sort_key)
 
         htmls = [ u'<h1>%s</h1><div class="blog_entries">'%self.title ]
-        htmls.extend(u'<div class="blog_entry">'+i.process()+'</div>' for i in reversed(items))
+        htmls.extend(u'<div class="blog_entry row" >%s</div>'%(i.process()) for i in reversed(items))
         htmls.append(u'</div>')
         return u'\n'.join(htmls)
 
